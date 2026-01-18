e78f417c323f6204db14caaaa2c42e17c0a6f190
Lautaro Blasco <lautaroblasco98@gmail.com>
2026-01-18 02:23:26 +0000

    added products to init.sql file

--- migrations/init.sql
+++ migrations/init.sql
@@ -23,5 +23,19 @@ CREATE TABLE IF NOT EXISTS auths (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS products (
+    id VARCHAR(36) PRIMARY KEY,
+    store_id VARCHAR(36) NOT NULL REFERENCES stores(id) ON DELETE CASCADE,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    stock INTEGER NOT NULL DEFAULT 0 CHECK (stock >= 0),
+    price DECIMAL(10, 2) NOT NULL CHECK (price > 0),
+    image_url TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE INDEX IF NOT EXISTS idx_auths_email ON auths(email);
 CREATE INDEX IF NOT EXISTS idx_auths_account ON auths(account_id, account_type);
+CREATE INDEX IF NOT EXISTS idx_products_store_id ON products(store_id);
+CREATE INDEX IF NOT EXISTS idx_products_name ON products(name);
