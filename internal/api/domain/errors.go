1f4bc30c01ccc9801fa4eb00f327731bcabd040a
Lautaro Blasco <lautaroblasco98@gmail.com>
2026-01-18 02:14:08 +0000

    updated errors file, added some pre-defined errors that could be useful

--- internal/api/domain/errors.go
+++ internal/api/domain/errors.go
@@ -1,5 +1,7 @@
 package domain
 
+import "errors"
+
 type InvalidPayloadError struct {
 	Fields map[string]string
 }
@@ -7,3 +9,12 @@ type InvalidPayloadError struct {
 func (e InvalidPayloadError) Error() string {
 	return "invalid payload"
 }
+
+var (
+	ErrInvalidProductName        = errors.New("product name cannot be empty")
+	ErrInvalidProductDescription = errors.New("product description cannot be empty")
+	ErrInvalidStock              = errors.New("stock cannot be negative")
+	ErrInvalidPrice              = errors.New("price must be greater than zero")
+	ErrInvalidStoreID            = errors.New("store id cannot be empty")
+	ErrProductNotFound           = errors.New("product not found")
+)
