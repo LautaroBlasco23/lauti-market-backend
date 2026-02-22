487e6c1b8b93daef1553d6b5e54b214c9df9cd50
Lautaro Blasco <lautaroblasco98@gmail.com>
2026-02-22 15:46:06 +0000

    moved auth errors into errors fie

--- internal/api/domain/errors.go
+++ internal/api/domain/errors.go
@@ -11,6 +11,16 @@ func (e InvalidPayloadError) Error() string {
 }
 
 var (
+	// Auth errors
+	ErrInvalidEmail       = errors.New("invalid email")
+	ErrInvalidPassword    = errors.New("password must be at least 8 characters")
+	ErrInvalidAccountID   = errors.New("account ID cannot be empty")
+	ErrInvalidAccountType = errors.New("invalid account type")
+	ErrAuthNotFound       = errors.New("auth not found")
+	ErrEmailExists        = errors.New("email already exists")
+	ErrInvalidCredentials = errors.New("invalid credentials")
+
+	// Product errors
 	ErrInvalidProductName        = errors.New("product name cannot be empty")
 	ErrInvalidProductDescription = errors.New("product description cannot be empty")
 	ErrInvalidStock              = errors.New("stock cannot be negative")
